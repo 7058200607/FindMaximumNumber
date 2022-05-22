1d7d0403299ab27fe41f7e74877c0845dde8f8bd
vrushali joshi <vrushalijoshi589@mail.com>
2022-05-22 15:19:38 +0530

    Find Maximum Number Of given threee Value

---
+++ FindMaximumNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumNumber
+{
+    public class program 
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(" Find The Maximum Number!");
+           int output =MaximumNumber.AMaximumNumber(11, 22, 33);
+          Console.WriteLine(output);
+           
+
+
+        }
+
+
+    }
+}
+
